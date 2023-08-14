03dea70dfe5c6bbc30d034bf7b29d6ab47bcb830
genkinomori <pansda@gmail.com>
2023-08-14 14:36:48 +0800

    Make archive to keep mod

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR > /dev/null
 
+rm -rf ./build
 mkdir -p ./build
 
 echo "Building for linux..."
@@ -40,5 +41,15 @@ GOOS=darwin GOARCH=arm64 go build
 cd ../..
 mv cmd/decoder/decoder ./build/decoder-darwin-apple-silicon
 mv cmd/encoder/encoder ./build/encoder-darwin-apple-silicon
+
+echo "Making archive files..."
+cd ./build
+tar -cvf decoder-darwin-apple-silicon.tar ./decoder-darwin-apple-silicon
+tar -cvf encoder-darwin-apple-silicon.tar ./encoder-darwin-apple-silicon
+tar -cvf decoder-darwin-amd64.tar ./decoder-darwin-amd64
+tar -cvf encoder-darwin-amd64.tar ./encoder-darwin-amd64
+tar -cvf decoder-linux-amd64.tar ./decoder-linux-amd64
+tar -cvf encoder-linux-amd64.tar ./encoder-linux-amd64
+rm decoder-darwin-apple-silicon encoder-darwin-apple-silicon decoder-darwin-amd64 encoder-darwin-amd64 decoder-linux-amd64 encoder-linux-amd64
 
 popd > /dev/null
